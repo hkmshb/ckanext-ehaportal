08c32020a22f56f3deb521ec90a82496ff165ff7
hkmshb <s.abdulhakeeem@gmail.com>
2023-11-18 10:12:53 +0100

    customize home page with some empty placeholder sections

--- ckanext/ehaportal/views/home.py
+++ ckanext/ehaportal/views/home.py
@@ -6,25 +6,39 @@ from flask import Blueprint
 eha_home = Blueprint('eha_home', __name__)
 
 
-def impacts():
+def index():
+    '''display home page'''
+    extra_vars = {
+        'sector_count': 12,
+        'dataset_count': 500,
+        'stat': {
+            'total_website_visits': 1000,
+            'total_generated_maps': 300,
+            'total_downloaded_datasets': 20000,
+        }
+    }
+    return base.render(u'home/index.html', extra_vars=extra_vars)
+
+
+def impact():
     '''display impact page'''
     return base.render('home/impact.html', extra_vars={})
 
 
 def support():
     '''display support page'''
-    # TODO: drop hard-coded values, extend/use config page instead 
+    # TODO: drop hard-coded values, extend/use config page instead
     support = {
-        'number': '0909-399-5400', 
+        'number': '0909-399-5400',
         'number_url': 'tel:+2349093995400',
         'email_url': 'mailto:dataportal.support@ehealthafrica.org',
     }
-
     return base.render('home/support.html', extra_vars={'support': support})
 
 
 util_rules = [
-    ('/impacts', impacts),
+    ('/', index),
+    ('/impact', impact),
     ('/support', support)
 ]
 
